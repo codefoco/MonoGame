15e04411776372c81b65a85ddd34e8098bdb1bf3
riperiperi <rhy3756547@hotmail.com>
2025-12-26 17:34:43 +0000

    Use HashSet to track GraphicsDevice resources (#9027)
    <!--
    Are you targeting develop? All PRs should target the develop branch
    unless otherwise noted.
    -->
    
    Improves performance of `GraphicsDevice.RemoveResourceReference` (and by
    extension `GraphicsResource.Dispose`) when there are a lot of resources.
    
    <!--
    Please try to make sure that there is a bug logged for the issue being
    fixed if one is not present.
    Especially for issues which are complex. 
    The bug should describe the problem and how to reproduce it.
    -->
    
    ### Description of Change
    
    `List<T>.Remove(T item)` needs to iterate every item of the list to find
    the one to remove, then shift items when removing from the middle.
    HashSet<T> has much faster deletion, at the cost of a slightly slower
    insert.
    
    My specific example had over 30000 resources, and triggered large
    stutters when clearing a cache with around 4000 recently allocated
    items. It was iterating approximately 120000000 resources removing these
    entries, since they would be at the end of the list.
    
    My test case was a little extreme since I was scrolling a map with a
    huge number of unique models that stream in+out, but it's an easy
    improvement to make.
    
    <!-- 
    Enter description of the fix in this section.
    Please be as descriptive as possible, future contributors will need to
    know *why* these changes are being made.
    For inspiration review the commit/PR history in the MonoGame repository.
    -->

--- MonoGame.Framework/Graphics/GraphicsDevice.cs
+++ MonoGame.Framework/Graphics/GraphicsDevice.cs
@@ -8,6 +8,7 @@ using System.Diagnostics;
 using System.Globalization;
 using MonoGame.Framework.Utilities;
 using System.Runtime.InteropServices;
+using System.Linq;
 
 
 namespace Microsoft.Xna.Framework.Graphics
@@ -136,7 +137,7 @@ namespace Microsoft.Xna.Framework.Graphics
         // Use WeakReference for the global resources list as we do not know when a resource
         // may be disposed and collected. We do not want to prevent a resource from being
         // collected by holding a strong reference to it in this list.
-        private readonly List<WeakReference> _resources = new List<WeakReference>();
+        private readonly HashSet<WeakReference> _resources = new HashSet<WeakReference>();
 
 		// TODO Graphics Device events need implementing
 		public event EventHandler<EventArgs> DeviceLost;
@@ -702,7 +703,7 @@ namespace Microsoft.Xna.Framework.Graphics
                 }
 
                 // Remove references to resources that have been garbage collected.
-                _resources.RemoveAll(wr => !wr.IsAlive);
+                _resources.RemoveWhere(wr => !wr.IsAlive);
             }
         }
 
